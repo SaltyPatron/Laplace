ca8b89c193db6bed4a830b1083bbfa5d538977e6
SaltyPatron <thesaltypatron@gmail.com>
2026-09-01 13:42:15 -0500

    Compile chess eval write-isolation regression

--- app/Laplace.Endpoints.OpenAICompat.Tests/ChessRuntimeContractTests.cs
+++ app/Laplace.Endpoints.OpenAICompat.Tests/ChessRuntimeContractTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Laplace.Chess.Service;
+using Laplace.Modality.Chess;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
