f53fb137aa48a0848709ae03b899c8a2589a9be5
SaltyPatron <thesaltypatron@gmail.com>
2026-09-17 15:54:37 -0500

    Load operator receipt presentation rules

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import { TooltipProvider } from '@ui';
 import '@ui/layers.css';
 import '@ui/theme.css';
+import './admin/operator-receipts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
