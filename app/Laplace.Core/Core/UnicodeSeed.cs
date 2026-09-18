b9c6172ff46b29984ddc980b868acc2b25d42c6d
SaltyPatron <thesaltypatron@gmail.com>
2026-09-18 07:50:19 -0500

    Remove managed T0 geometry recomputation API

--- app/Laplace.Core/Core/UnicodeSeed.cs
+++ app/Laplace.Core/Core/UnicodeSeed.cs
@@ -4,27 +4,6 @@ public static unsafe class UnicodeSeed
 {
     public const int CodepointCount = 0x110000;
 
-    public static void Compute(string ucdxmlPath, string ducetPath, Span<CodepointRecord> outRecords)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(ucdxmlPath);
-        ArgumentException.ThrowIfNullOrEmpty(ducetPath);
-        if (outRecords.Length < CodepointCount)
-            throw new ArgumentException($"buffer must hold at least {CodepointCount} records", nameof(outRecords));
-        fixed (CodepointRecord* p = outRecords)
-        {
-            int rc = NativeInterop.UnicodeSeedCompute(ucdxmlPath, ducetPath, p, (nuint)outRecords.Length);
-            if (rc != 0) throw new InvalidOperationException(
-                $"laplace_unicode_seed_compute(\"{ucdxmlPath}\", \"{ducetPath}\") returned {rc}");
-        }
-    }
-
-    public static CodepointRecord[] Compute(string ucdxmlPath, string ducetPath)
-    {
-        var buf = new CodepointRecord[CodepointCount];
-        Compute(ucdxmlPath, ducetPath, buf);
-        return buf;
-    }
-
     /// <summary>Read/decompress and parse one UCD XML artifact to completion.</summary>
     public static void ValidateUcdXml(string ucdxmlPath)
     {
